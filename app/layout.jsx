4fd2ef6afe72aa6144261e83c20c8b2231c28fac
Astha2044 <astha.200404@gmail.com>
2026-05-12 10:25:50 +0530

    feat: add responsive navbar component styles and implement base layout structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import FloatingActions from "../components/FloatingActions";
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export const metadata = {
   metadataBase: new URL('https://www.vsnaturalsandessentials.com'),
   title: {
